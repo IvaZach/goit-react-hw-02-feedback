40c353bbe3d734bbf3190d77894e13fe6eb1ef16
IvaZach <ivazacherniuk@gmail.com>
2023-12-08 15:00:52 +0300

    add part 3 and 4

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,90 +1,24 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class Feedback extends Component {
-  state = { good: 0, neutral: 0, bad: 0 };
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div>
+    {options.map(nameKey => (
+      <button
+        key={nameKey}
+        name={nameKey}
+        onClick={onLeaveFeedback}
+        type="button"
+        className="btn btn-outline-primary"
+      >
+        {nameKey}
+      </button>
+    ))}
+  </div>
+);
 
-  onClickGood = () => {
-    this.setState(prevState => ({ good: prevState.good + 1 }));
-  };
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
 
-  onClickNeutral = () => {
-    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
-  };
-
-  onClickBad = () => {
-    this.setState(prevState => ({ bad: prevState.bad + 1 }));
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    return total ? Math.round((good * 100) / total) : 0;
-  };
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const percent = this.countPositiveFeedbackPercentage();
-    return (
-      <div>
-        <h1 className="display-1">Please live feedback</h1>
-        <button
-          type="button"
-          className="btn btn-outline-primary"
-          onClick={this.onClickGood}
-          arial-label="good"
-        >
-          Good
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-primary"
-          onClick={this.onClickNeutral}
-          arial-label="neutral"
-        >
-          Neutral
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-primary"
-          onClick={this.onClickBad}
-          arial-label="bad"
-        >
-          Bad
-        </button>
-
-        <h2 className="display-4">Statistics</h2>
-        <table className="table">
-          <tbody>
-            <tr>
-              <th scope="row">Good:</th>
-              <td>{good}</td>
-            </tr>
-            <tr>
-              <th scope="row">Neutral:</th>
-              <td>{neutral}</td>
-            </tr>
-            <tr>
-              <th scope="row">Bad:</th>
-              <td>{bad}</td>
-            </tr>
-            <tr>
-              <th scope="row">Total:</th>
-              <td>{total}</td>
-            </tr>
-            <tr>
-              <th scope="row">PositiveFeedback:</th>
-              <td>{percent}%</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-
-export default Feedback;
+export default FeedbackOptions;
